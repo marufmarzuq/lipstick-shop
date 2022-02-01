333445e41ae79b133d7dedcf89c164ab9541bfa4
marufmarzuq <marufmarzuq18@gmail.com>
2022-02-02 02:29:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import AuthProvider from "./Context/AuthProvider/AuthProvider";
+import Reg from "./pages/login/Reg";
+import Login from "./pages/login/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AuthProvider>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Reg />} />
+                </Routes>
+                <Footer />
+            </div>
+        </AuthProvider>
+    );
 }
 
 export default App;
